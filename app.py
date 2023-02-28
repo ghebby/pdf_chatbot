0acc5dc21ebada48b0a2910cf3cd831285a37f4b
AenishShrestha <84612270+AenishShrestha@users.noreply.github.com>
2023-02-28 20:52:29 +0545

    Update app.py

--- app.py
+++ app.py
@@ -16,9 +16,9 @@ st.title("PDF to chatbot")
 
 st.write("This app will help you to create a chatbot from a PDF file")
 
-# st.subheader("Step 1: 🔑 Setup your OpenAI API Key")
+st.subheader("Step 1: 🔑 Setup your OpenAI API Key")
 # ask for a user text input
-# user_openai_api_key = st.text_input("Enter your OpenAI API Key",placeholder="OPENAI_API_KEY",value="")
+user_openai_api_key = st.text_input("Enter your OpenAI API Key",placeholder="OPENAI_API_KEY",value="")
 # st.write("You can get yours from here - https://beta.openai.com/account/api-keys")
 
 st.subheader("Step 2: 📤 Upload your PDF file")
@@ -26,7 +26,10 @@ uploaded_file = st.file_uploader("Choose a file")
 
 user_openai_api_key = st.secrets["user_api_key"]
 
-if uploaded_file is None and user_openai_api_key is None:
+if user_openai_api_key is None:
+    st.write("Error : ❌ No Api Key Found")
+
+if uploaded_file is None:
     st.write("Error : ❌ No Pdf File Found")
 # if uploaded_file is not None and user_openai_api_key is not None:
 if uploaded_file is not None and user_openai_api_key is not None:
