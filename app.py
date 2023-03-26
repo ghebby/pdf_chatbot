7bf1b575ba10138e37c6f559ccf6f1f502e97594
AenishShrestha <84612270+AenishShrestha@users.noreply.github.com>
2023-03-26 08:48:12 +0545

    testing for 1 million character limit

--- app.py
+++ app.py
@@ -98,7 +98,7 @@ if uploaded_file is not None and user_openai_api_key is not None:
     
     st.write("Characters : ",len(raw_text))
 
-    rate_limit_pdf = 500000
+    rate_limit_pdf = 1000000
 
     if len(raw_text)>rate_limit_pdf:
         st.warning("Sorry Your File Is Too Big. 😔")
